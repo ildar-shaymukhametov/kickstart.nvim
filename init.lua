d65dd3e0a137483e673df4ded16b5c93c42757c0
Ildar Shaymukhametov <ildar.shaymukhametov@gmail.com>
2025-10-21 16:51:02 +0500

    to fuzzy refine

--- init.lua
+++ init.lua
@@ -412,11 +412,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-f>'] = 'to_fuzzy_refine' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
