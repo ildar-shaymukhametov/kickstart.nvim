dbd28e89e99913a054365632b15417f2b3086bc3
Ildar Shaymukhametov <ildar.shaymukhametov@gmail.com>
2025-10-31 11:58:56 +0500

    open multiple buffers from telescope

--- init.lua
+++ init.lua
@@ -406,6 +406,23 @@ require('lazy').setup({
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
 
+      -- Added a helper to open multiple files if more than one is selected
+      -- see https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require('telescope.actions').close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+          end
+        else
+          require('telescope.actions').select_default(prompt_bufnr)
+        end
+      end
+
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
@@ -414,7 +431,10 @@ require('lazy').setup({
         --
         defaults = {
           mappings = {
-            i = { ['<C-f>'] = 'to_fuzzy_refine' },
+            i = {
+              ['<C-f>'] = 'to_fuzzy_refine',
+              ['<CR>'] = select_one_or_multi,
+            },
           },
         },
         -- pickers = {}
