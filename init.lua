af8fe3fd7e803a7d55353e01a49926122c2dcc87
Ildar Shaymukhametov <ildar.shaymukhametov@gmail.com>
2025-11-01 12:57:05 +0500

    mini.surround: change keybindings

--- init.lua
+++ init.lua
@@ -998,7 +998,16 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'cla',
+          delete = 'cld',
+          find = 'clf',
+          find_left = 'clF',
+          highlight = 'clh',
+          replace = 'clr',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
