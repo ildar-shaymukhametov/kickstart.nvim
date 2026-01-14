c38ca255d52c281854d8298dcb59ba619f49b666
Ildar Shaymukhametov <ildar.shaymukhametov@gmail.com>
2026-01-14 06:47:33 +0500

    roslyn

--- init.lua
+++ init.lua
@@ -791,40 +791,9 @@ require('lazy').setup({
   {
     'seblyng/roslyn.nvim',
     ft = 'cs',
-    opts = function()
-      return {
-        config = {
-          settings = {
-            ['csharp|background_analysis'] = {
-              dotnet_analyzer_diagnostics_scope = 'fullSolution',
-              dotnet_compiler_diagnostics_scope = 'fullSolution',
-            },
-            ['csharp|code_lens'] = {
-              dotnet_enable_references_code_lens = true,
-            },
-            ['csharp|completion'] = {
-              dotnet_show_name_completion_suggestions = true,
-              dotnet_show_completion_items_from_unimported_namespaces = true,
-              dotnet_provide_regex_completions = true,
-            },
-            ['csharp|inlay_hints'] = {
-              csharp_enable_inlay_hints_for_implicit_object_creation = true,
-              csharp_enable_inlay_hints_for_implicit_variable_types = true,
-              csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-              csharp_enable_inlay_hints_for_types = true,
-              dotnet_enable_inlay_hints_for_indexer_parameters = true,
-              dotnet_enable_inlay_hints_for_literal_parameters = true,
-              dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-              dotnet_enable_inlay_hints_for_other_parameters = true,
-              dotnet_enable_inlay_hints_for_parameters = true,
-              dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-              dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-              dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-            },
-          },
-        },
-      }
-    end,
+    opts = {
+      filewatching = 'off',
+    },
   },
   { -- Autoformat
     'stevearc/conform.nvim',
